00c9d2eec633433ed76a62692196395a732bda79
Marat <94042173+Crescent17@users.noreply.github.com>
2023-07-22 18:52:57 +0300

    close modal on escape pressed

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ btnsOpenModal.forEach(btn => {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
